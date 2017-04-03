3d64338834be2d193592bd26e7506de4ced57e41
Binh Phan <phanphuongbinhspk@gmail.com>
2017-04-03 10:54:08 +0700

    Create folder "example_bash_script"

---
+++ example_bash_script/check_temp.sh
@@ -0,0 +1,11 @@
+cpu=$(</sys/class/thermal/thermal_zone0/temp)
+echo "$(date) @ $(hostname)"
+echo "-----------------------------------------------"
+echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
+echo "CPU => $((cpu/1000))'C"
+
+sleep 1
+
+clear
+
+./check_temp.sh
